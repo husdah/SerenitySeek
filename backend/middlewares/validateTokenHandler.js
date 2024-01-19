82838d0b8b019917919ac4e76f20171a3551b52e
hayatfaily <haya.toufayli.2020@outlook.com>
2024-01-19 19:43:01 +0200

    mmmmmmmmmmmmmmmmm

---
+++ backend/middlewares/validateTokenHandler.js
@@ -0,0 +1,23 @@
+const jwt=require('jsonwebtoken');
+
+const validateToken=async(req,res,next)=>{
+    let token;
+    let authHeader=req.header.Authorization || req.header.authorization;
+    if(authHeader && authHeader.startsWith("Bearer"))
+    {
+        token=authHeader.split(" ")[1];
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+            if(err)
+            {
+                res.status(400).json({error:"User is not authorized"})
+            }
+            req.user=decoded.user;
+            next();
+        })
+    }
+    if(!token)
+    {
+        res.status(400).json({error:"Missing token"})
+    }
+}
+module.exports=validateToken;
