04fb8e8a869950966eee4287ad52dcbccb310d2f
ZeinabHijazi <hijazizeinab5@gmail.com>
2024-01-20 16:32:35 +0200

    aaaa

--- backend/routes/hotelRouter.js
+++ backend/routes/hotelRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const upload = require('../middlewares/multerMiddleware');
-
 const { addHotel, deleteHotel, getHotelsByCompanyId } = require('../controllers/hotelController');
+const upload = require('../middlewares/multerMiddleware');
+const validateToken = require('../middlewares/validateTokenHandler');
 
+
+router.use(validateToken);
 router.post("/addHotel", upload.array('gallery'), addHotel);
 router.delete("/delHotel/:id", deleteHotel);
 router.get("/getHotels", getHotelsByCompanyId);
