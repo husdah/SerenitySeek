a8787f9c37707ae79b5e4357ad7980bc27478241
Hussein Daher <husdaher579@gmail.com>
2024-02-11 17:54:54 +0200

    nav

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,22 +1,24 @@
 import {useRef} from 'react';
 import {FaTimes,FaBars} from 'react-icons/fa';
-import './navbar.css';
+import styles from './navbar.module.css';
+import Logo from '../../assets/images/LogoNoBg.png'
 const Navbar = () => {
     const navRef =useRef();
     const showNavbar=()=>{
         navRef.current.classList.toggle("responsive_nav");
     }
     return ( 
-        <div className="navbar">
-            <header>
-                <h3>Logo</h3>
-                <nav ref={navRef}>
+        <div className={styles.navbar}>
+            <header className={styles.header}>
+                {/* <h3>Logo</h3> */}
+                <img src={Logo}></img>
+                <nav className={styles.nav} ref={navRef}>
                     <a href="#">Home</a>
                     <a href="#">Packages</a>
                     <a href="#">About us</a>
                     <a href="#">Contact us</a>
-                    <button className='nav-btn nav-close-btn' onClick={showNavbar} ><FaTimes/></button>
-                    <button className='nav-btn'  onClick={showNavbar}><FaBars/></button>
+                    <button className={styles.nav_btn} onClick={showNavbar} ><FaTimes/></button>
+                    <button className={styles.nav_btn}  onClick={showNavbar}><FaBars/></button>
                 </nav>
                 <button>Login</button>
             </header>
