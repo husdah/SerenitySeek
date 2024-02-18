e513153f19e6e4b3766d0acf69e6bcd8a319c617
Hussein Daher <husdaher579@gmail.com>
2024-02-18 13:27:11 +0200

    chat comp

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { default: axios } = require("axios");
 const validateToken = require('../middlewares/validateTokenHandler');
+require('dotenv').config();
 
 router.post("/authenticate", validateToken , async (req, res) => {
     const { username } = req.body;
@@ -10,7 +11,7 @@ router.post("/authenticate", validateToken , async (req, res) => {
       const r = await axios.put(
         "https://api.chatengine.io/users/",
         { username: username, secret: username, first_name: username },
-        { headers: { "Private-Key": "fbbd2da5-885a-49ff-8a1a-91d3f3ccc33a" } }
+        { headers: { "Private-Key":  process.env.CHAT_APP_PRIVATE_KEY } }
       );
       return res.status(r.status).json(r.data);
     } catch (e) {
