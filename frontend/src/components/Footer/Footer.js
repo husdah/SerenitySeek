83310b77227390a38c03dfb1be0efde702369d90
Hussein Daher <husdaher579@gmail.com>
2024-02-20 19:03:56 +0200

    haya

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,16 +2,17 @@ import React  from 'react'
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 import logo from '../../assets/images/LogoNoBg.png'
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaPhone } from "react-icons/fa";
 import { BiLogoInstagramAlt } from "react-icons/bi";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import { FaSquareXTwitter, FaLocationDot } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
         <div className={styles.footer_content}>
             <img src={logo} alt="" />
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptatem quidem perferendis obcaecati atque</p>
+            <p>Serenity Seek: Your ultimate destination for revolutionizing corporate travel with seamless solutions.</p>
             <div className={styles.icons}>
                <Link to="#" className={styles.link_icon}><FaFacebook className={styles.icon} /></Link>
                <Link to="#" className={styles.link_icon}><BiLogoInstagramAlt className={styles.icon} /></Link>
@@ -19,25 +20,25 @@ export default function Footer() {
             </div>
         </div>
         <div className={styles.footer_content}>
-            <h4>Content</h4>
-            <li><Link to="#" className={styles.link}>About</Link></li>
-            <li><Link to="#" className={styles.link}>Packages</Link></li>
-            <li><Link to="#" className={styles.link}>Companies</Link></li>
-            <li><Link to="#" className={styles.link}>Logout</Link></li> 
+            <h4>Quick Links</h4>
+            <li><Link to="/" className={styles.link}>Home</Link></li>
+            <li><Link to="/Package" className={styles.link}>Packages</Link></li>
+            <li><Link to="/allBlogs" className={styles.link}>Blogs</Link></li>
+            <li><Link to="/TermsOfServices" className={styles.link}>Terms of Services</Link></li> 
         </div>
         <div className={styles.footer_content}>
-            <h4>Content</h4>
-            <li><Link to="#" className={styles.link}>About</Link></li>
-            <li><Link to="#" className={styles.link}>Packages</Link></li>
-            <li><Link to="#" className={styles.link}>Companies</Link></li>
-            <li><Link to="#" className={styles.link}>Logout</Link></li> 
+            <h4>Join Us</h4>
+            <li><Link to="/Login" className={styles.link}>Login</Link></li>
+            <li><Link to="/Register" className={styles.link}>Register</Link></li>
+            <li><Link to="/chatApp" className={styles.link}>Chat Community</Link></li>
+            <li><Link to="/LogoutAndRedirect" className={styles.link}>Logout</Link></li> 
         </div>
         <div className={styles.footer_content}>
-            <h4>Content</h4>
-            <li><Link to="#" className={styles.link}>About</Link></li>
-            <li><Link to="#" className={styles.link}>Packages</Link></li>
-            <li><Link to="#" className={styles.link}>Companies</Link></li>
-            <li><Link to="#" className={styles.link}>Logout</Link></li> 
+            <h4>Keep In Touch</h4>
+            <li><Link to="/contact" className={styles.link}>Contact Us</Link></li>
+            <li className={styles.link}><FaLocationDot className={styles.icon} /> Nabatieh</li>
+            <li className={styles.link}><FaPhone className={styles.icon} /> +961/ 123456</li>
+            <li className={styles.link}><IoMdMail className={styles.icon} /> serenityseek2024@gmail.com</li> 
         </div>
     </div>
   )
